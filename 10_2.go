4980d1f5e3a37f4fd3ac4e2aedc4817170bdeb65
Kasper Middelboe Petersen <me@phant.dk>
2015-12-21 21:03:16 +0100

    Added solution for 10_2

---
+++ 10_2.go
@@ -0,0 +1,62 @@
+package main
+
+/*
+--- Day 10: Elves Look, Elves Say ---
+
+Today, the Elves are playing a game called look-and-say. They take turns making sequences by reading aloud the previous sequence and using that reading as the next sequence. For example, 211 is read as "one two, two ones", which becomes 1221 (1 2, 2 1s).
+
+Look-and-say sequences are generated iteratively, using the previous value as input for the next step. For each step, take the previous value, and replace each run of digits (like 111) with the number of digits (3) followed by the digit itself (1).
+
+For example:
+
+1 becomes 11 (1 copy of digit 1).
+11 becomes 21 (2 copies of digit 1).
+21 becomes 1211 (one 2 followed by one 1).
+1211 becomes 111221 (one 1, one 2, and two 1s).
+111221 becomes 312211 (three 1s, two 2s, and one 1).
+Starting with the digits in your puzzle input, apply this process 40 times. What is the length of the result?
+
+Your puzzle input is 1113222113.
+
+Your puzzle answer was 252594.
+
+--- Part Two ---
+
+Neat, right? You might also enjoy hearing John Conway talking about this sequence (that's Conway of Conway's Game of Life fame).
+
+Now, starting again with the digits in your puzzle input, apply this process 50 times. What is the length of the new result?
+
+*/
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var input = []byte(`1113222113`)
+
+func main() {
+	for j := 0; j < 50; j++ {
+		prevChar := byte('x')
+		prevCount := 0
+		newInput := make([]byte, 0, 1000000)
+		for i := 0; i < len(input); i++ {
+			if prevChar != input[i] {
+				if prevChar != byte('x') {
+					newInput = append(newInput, []byte(strconv.Itoa(prevCount))...)
+					newInput = append(newInput, prevChar)
+				}
+				prevChar = input[i]
+				prevCount = 1
+			} else {
+				prevCount++
+			}
+		}
+
+		newInput = append(newInput, []byte(strconv.Itoa(prevCount))...)
+		newInput = append(newInput, prevChar)
+
+		input = newInput
+		fmt.Println(j, len(input))
+	}
+}
