7d78fff4d6cce0985d60c02f2c9aade336a9931f
Kasper Middelboe Petersen <kasper@sybogames.com>
2017-12-17 21:08:39 +0100

    Day 7

---
+++ 2017/07_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewTower(t *testing.T) {
+	testCases := []struct {
+		In     string
+		Result string
+	}{
+		{`pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)`, "tknk"},
+	}
+
+	for i, testCase := range testCases {
+		assert.Equal(t, testCase.Result, NewTower(testCase.In).Name, "(case=%d)", i)
+	}
+
+	NewTower(testCases[0].In).Print()
+	t.Fail()
+}
