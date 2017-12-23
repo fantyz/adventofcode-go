d078ad94a940e8fe7522317b6451bc1282560502
Kasper Middelboe Petersen <kasper@sybogames.com>
2017-12-23 01:47:45 +0100

    Finished day 18

--- 2017/18_test.go
+++ 2017/18_test.go
@@ -26,3 +26,22 @@ jgz a -2`, 4},
 		assert.Equal(t, testCase.Result, NewProgram(testCase.In).RunUntilRcv(), "(case=%d)", i)
 	}
 }
+
+func TestProgram2Run(t *testing.T) {
+	testCases := []struct {
+		In     string
+		Result int
+	}{
+		{`snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d`, 3},
+	}
+
+	for i, testCase := range testCases {
+		assert.Equal(t, testCase.Result, RunInParallel(testCase.In), "(case=%d)", i)
+	}
+}
