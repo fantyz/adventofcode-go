ad4a439bc34b8d845e5b04732326dfa6f91cf4b7
Kasper Middelboe Petersen <kasper.middelboe.petersen@maersk.com>
2018-12-12 17:37:54 +0100

    WIP

--- 2018/12.go
+++ 2018/12.go
@@ -12,20 +12,41 @@ import (
 
 func main() {
 	fmt.Println("Day 12: Subterranean Sustainability")
+	p := NewPots(initialPuzzleState, rules)
+	p.Ticks(20)
+	fmt.Println(" > Sum after 20 generations:", p.Sum())
+	p.Ticks(50000000000 - 20)
+	fmt.Println(" > Sum after 50000000000 generations:", p.Sum())
+}
+
+type Pot struct {
+	next, prev *Pot
+	id         int
+	val        bool
+	prevVal bool
 }
 
 type Pots struct {
-	pots  []bool
-	zero  int
+	pots  *Pot
 	rules []*Rule
 }
 
 func NewPots(initialPots string, rules string) *Pots {
-	p := Pots{
-		pots: make([]bool, len(initialPots)),
+	pots := &Pots{
+		pots: &Pot{
+			id:  0,
+			val: initialPots[0] == '#',
+		},
 	}
-	for i := 0; i < len(initialPots); i++ {
-		p.pots[i] = initialPots[i] == '#'
+	tail := pots.pots
+
+	for i := 1; i < len(initialPots); i++ {
+		tail.next = &Pot{
+			prev: tail,
+			id:   i,
+			val:  initialPots[i] == '#',
+		}
+		tail = tail.next
 	}
 	for _, l := range strings.Split(rules, "\n") {
 		if len(l) <= 0 {
@@ -44,27 +65,90 @@ func NewPots(initialPots string, rules string) *Pots {
 	return &p
 }
 
+func (p *Pots) Ticks(n int) *Pots {
+	for i := 0; i < n; i++ {
+		if i%1000000 == 0 && i > 0 {
+			fmt.Print(".")
+		}
+		p.Tick()
+	}
+	fmt.Println()
+	return p
+}
+
 func (p *Pots) Tick() *Pots {
-	new := make([]bool, len(p.pots)+4)
-	for i := 0; i < len(p.pots); i++ {
+	for 
+
+	
+	new := make([]bool, len(p.pots)+8)
+	for i := 0; i < len(new); i++ {
+		p1, p2, p3, p4, p5 := false, false, false, false, false
+		if i-2-4 >= 0 && i-2-4 < len(p.pots) {
+			p1 = p.pots[i-2-4]
+		}
+		if i-1-4 >= 0 && i-1-4 < len(p.pots) {
+			p2 = p.pots[i-1-4]
+		}
+		if i-4 >= 0 && i-4 < len(p.pots) {
+			p3 = p.pots[i-4]
+		}
+		if i+1-4 >= 0 && i+1-4 < len(p.pots) {
+			p4 = p.pots[i+1-4]
+		}
+		if i+2-4 >= 0 && i+2-4 < len(p.pots) {
+			p5 = p.pots[i+2-4]
+		}
+
 		for _, r := range p.rules {
-			if new[i-2] == r.cond[0] && new[i-1] == r.cond[1] && new[i] == r.cond[2] && new[i+1] == r.cond[3] && new[i+2] == r.cond[4] {
-				new[i] = 
+			if p1 == r.cond[0] && p2 == r.cond[1] && p3 == r.cond[2] && p4 == r.cond[3] && p5 == r.cond[4] {
+				new[i] = r.res
+				break
 			}
 		}
 	}
 
+	first := len(new)
+	last := 0
+	for i := range new {
+		if new[i] {
+			if first > i {
+				first = i
+			}
+			if last < i {
+				last = i
+			}
+		}
+	}
+
+	p.pots = new[first : last+1]
+
+	p.zero += 4 - first
+
 	return p
+}
+
+func (p *Pots) Sum() int {
+	sum := 0
+	cur = p.pots
+	for cur != nil {
+		if cur.val {
+			sum += cur.id
+		}
+		cur = cur.next
+	}
+	return sum
 }
 
 func (p *Pots) String() string {
 	str := ""
-	for i := range p.pots {
-		if p.pots[i] {
+	cur = p.pots
+	for cur != nil {
+		if cur.val {
 			str += "#"
 		} else {
 			str += "."
 		}
+		cur = cur.next
 	}
 	return str
 }
--- 2018/12_test.go
+++ 2018/12_test.go
@@ -10,6 +10,7 @@ func TestNewPots(t *testing.T) {
 	testCases := []struct {
 		Pots  string
 		Rules string
+		Ticks []string
 	}{
 		{
 			Pots: `#..#.#..##......###...###`,
@@ -27,10 +28,54 @@ func TestNewPots(t *testing.T) {
 ###.. => #
 ###.# => #
 ####. => #`,
+			Ticks: []string{
+				"#..#.#..##......###...###",
+				"#...#....#.....#..#..#..#",
+				"##..##...##....#..#..#..##",
+			},
 		},
 	}
 
 	for i, c := range testCases {
-		assert.Equal(t, c.Pots, NewPots(c.Pots, c.Rules).String(), "(case=%d)", i)
+		p := NewPots(c.Pots, c.Rules)
+		for _, exp := range c.Ticks {
+			assert.Equal(t, exp, p.String(), "(case=%d)", i)
+			p.Tick()
+		}
+	}
+}
+
+func TestSum(t *testing.T) {
+	testCases := []struct {
+		Pots   string
+		Rules  string
+		Ticks  int
+		ExpSum int
+	}{
+		{
+			Pots: `#..#.#..##......###...###`,
+			Rules: `...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #`,
+			Ticks:  20,
+			ExpSum: 325,
+		},
+	}
+
+	for i, c := range testCases {
+		p := NewPots(c.Pots, c.Rules)
+		p.Ticks(c.Ticks)
+		assert.Equal(t, c.ExpSum, p.Sum(), "(case=%d)", i)
 	}
 }
