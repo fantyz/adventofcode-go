0e5f6d3c544f280d1e2c8dff88b68bff7dd14094
Kasper Middelboe Petersen <kasper.middelboe.petersen@maersk.com>
2020-12-02 09:26:25 +0100

    Day 2 solved

--- 2020/02.go
+++ 2020/02.go
@@ -2,10 +2,125 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
-func init() { puzzles["2"] = func() { Day2(day2Input) } }
+func init() { puzzles["2"] = Day2 }
 
-func Day2(in string) {
-	fmt.Println("Day 2: ")
+/*
+--- Day 2: Password Philosophy ---
+Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+
+Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
+
+To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+
+How many passwords are valid according to their policies?
+
+Your puzzle answer was 416.
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+
+Your puzzle answer was 688.
+*/
+
+func Day2() {
+	fmt.Println("Day 2: Password Philosophy")
+	fmt.Println("Valid passwords:", ValidatePasswordFile(0, day2Input))
+	fmt.Println("Valid passwords (new policy):", ValidatePasswordFile(1, day2Input))
+}
+
+func ValidatePasswordFile(policy int, in string) int {
+	re := regexp.MustCompile(`^([0-9]+)-([0-9]+) ([a-z]+): ([a-z]+)$`)
+
+	validators := []func(int,int,string,string)bool{ValidatePassword, ValidatePassword2}
+	if policy < 0 || policy > len(validators) {
+		panic("unknown policy")
+	}
+
+	valid := 0
+	for n, s := range strings.Split(in, "\n") {
+		passwordEntry := re.FindStringSubmatch(s)
+		if len(passwordEntry) < 5 {
+			panic(fmt.Sprintf("Line %d did not match regexp", n))
+		}
+		min, err := strconv.Atoi(passwordEntry[1])
+		if err != nil {
+			panic(errors.Wrap(err, "failed to get min"))
+		}
+		max, err := strconv.Atoi(passwordEntry[2])
+		if err != nil {
+			panic(errors.Wrap(err, "failed to get max"))
+		}
+		seq := passwordEntry[3]
+		pass := passwordEntry[4]
+
+		if ok := validators[policy](min, max, seq, pass); !ok {
+			continue
+		}
+		valid++
+	}
+	return valid
+}
+
+func ValidatePassword(min, max int, seq string, pass string) bool {
+	n := strings.Count(pass, seq)
+	if n < min || n > max {
+		return false
+	}
+	return true
+}
+
+func ValidatePassword2(min, max int, seq string, pass string) bool {
+	if len(seq) != 1 {
+		panic("policy only support seq length of exactly 1")
+	}
+	// min and max is 1-indexed - lets zero index them
+	min = min-1
+	if min < 0 || min >= len(pass) {
+		return false
+	}
+
+	max = max-1
+	if max < 0 || max >= len(pass) {
+		return false
+	}
+
+	c := seq[0] // yes, no support for UTF8 or the likes to be found here!
+
+	if pass[min] != c && pass[max] != c {
+		return false
+	}
+	if pass[min] == c && pass[max] == c {
+		return false
+	}
+
+	return true
 }
--- 2020/02_test.go
+++ 2020/02_test.go
@@ -1,3 +1,61 @@
 package main
 
-import ()
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidatePassword(t *testing.T) {
+	testCases := map[string]struct {
+		Min, Max int
+		Seq      string
+		Pass     string
+		ExpValid   bool
+	}{
+		"example 1": {1, 3, "a", "abcde", true},
+		"example 2": {1, 3, "b", "cdefg", false},
+		"example 3": {2, 9, "c", "ccccccccc", true},
+	}
+
+	for n, c := range testCases {
+		valid := ValidatePassword(c.Min, c.Max, c.Seq, c.Pass)
+		if c.ExpValid {
+			assert.True(t, valid, n)
+		} else {
+			assert.False(t, valid, n)
+		}
+	}
+}
+
+func TestValidatePassword2(t *testing.T) {
+	testCases := map[string]struct {
+		Min, Max int
+		Seq      string
+		Pass     string
+		ExpValid   bool
+	}{
+		"example 1": {1, 3, "a", "abcde", true},
+		"example 2": {1, 3, "b", "cdefg", false},
+		"example 3": {2, 9, "c", "ccccccccc", false},
+	}
+
+	for n, c := range testCases {
+		valid := ValidatePassword2(c.Min, c.Max, c.Seq, c.Pass)
+		if c.ExpValid {
+			assert.True(t, valid, n)
+		} else {
+			assert.False(t, valid, n)
+		}
+	}
+}
+
+
+func TestValidatePasswordFile(t *testing.T) {
+	testCase := `1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`
+
+	assert.Equal(t, 2, ValidatePasswordFile(0, testCase), "Policy 0")
+	assert.Equal(t, 1, ValidatePasswordFile(1, testCase), "Policy 1")
+}
