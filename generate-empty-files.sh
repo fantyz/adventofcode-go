21843e867564ab2be833461876b6dd5badc7c294
Kasper Middelboe Petersen <kasper.middelboe.petersen@maersk.com>
2020-12-20 14:45:42 +0100

    Ease creation of new files.

---
+++ generate-empty-files.sh
@@ -0,0 +1,55 @@
+#!/bin/env bash
+
+set -e
+
+if [ "x$1" == "x" ] ||  [ "x$2" == "x" ]; then
+    echo "Usage:   ./generate-empty-files <path> <name>"
+    echo "Example: ./generate-empty-files 2020/ 25 would generate 2020/25.go, 2020/25_test.go and 2020/25_input.go"
+    exit 0
+fi
+
+DAY="${1%/}/${2}.go"
+TEST="${1%/}/${2}_test.go"
+INPUT="${1%/}/${2}_input.go"
+
+if [ -f "$DAY" ] || [ -f "$TEST" ] || [ -f "$INPUT" ]; then
+    echo "${DAY}, ${TEST} and/or ${INPUT} already exist!"
+    exit 1
+fi
+
+# <day>.go
+echo "package main
+
+import (
+       \"fmt\"
+)
+
+func init() { days[\"${2}\"] = Day${2} }
+
+/*
+ */
+
+func Day${2}() {
+	fmt.Println(\"\")
+}
+" > "${DAY}"
+
+# <day>_test.go
+echo "package main
+
+import (
+	\"testing\"
+)
+
+func TestDay${2}Pt1(t *testing.T) {
+}
+
+func TestDay${2}Pt2(t *testing.T) {
+}
+" > "${TEST}"
+
+# <day>_input.go
+echo "package main
+
+const day${2}Input = \`\`
+" > "${INPUT}"
