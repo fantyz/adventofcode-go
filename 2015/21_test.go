1b4d03d9120a2dc4201e1976f10f5df710cd20ea
Kasper Middelboe Petersen <kmi@milestone.dk>
2021-11-15 09:12:49 +0100

    Day 21

--- 2015/21_test.go
+++ 2015/21_test.go
@@ -14,8 +14,19 @@ func TestRPGSimulator20XX(t *testing.T) {
 }
 
 func TestDay21Pt1(t *testing.T) {
-
+	lowestToWin, _ := OptimizeRPGSimulator20XXGear(Character{
+		Hitpoints: day21InputHP,
+		Damage:    day21InputDamage,
+		Armor:     day21InputArmor,
+	})
+	assert.Equal(t, 91, lowestToWin)
 }
 
 func TestDay21Pt2(t *testing.T) {
+	_, highestToLoose := OptimizeRPGSimulator20XXGear(Character{
+		Hitpoints: day21InputHP,
+		Damage:    day21InputDamage,
+		Armor:     day21InputArmor,
+	})
+	assert.Equal(t, 158, highestToLoose)
 }
